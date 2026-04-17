f58857c6170a58259c1e40fb38544282e1df1423
l1senochek <lisenochek96@gmail.com>
2026-04-18 02:21:09 +0300

    refactor: moved LoadMore & useLoadMore

--- src/features/ProductsLoadMore/model/useLoadMore.ts
+++ src/features/ProductsLoadMore/model/useLoadMore.ts
@@ -1,19 +1,18 @@
 import { RefObject, useCallback, useLayoutEffect } from 'react';
-import { useAppDispatch, useAppSelector } from '../../../store/utils';
-import { useProducts } from '../../../store/hooks/useProducts';
+import { useAppDispatch, useAppSelector } from '../../../shared/store/utils';
+import { useProducts } from '../../../shared/store/hooks/useProducts';
 import {
 	productsActions,
 	productsSelectors,
-} from '../../../store/slices/products';
+} from '../../../shared/store/slices/products';
 
 interface UseLoadMoreParams {
 	ref: RefObject<HTMLDivElement>;
 }
+
 export const useLoadMore = ({ ref }: UseLoadMoreParams) => {
 	const dispatch = useAppDispatch();
-
 	const { products, isFetching, productsCount } = useProducts();
-
 	const page = useAppSelector(productsSelectors.getPage);
 
 	const isEndOfList = products.length >= productsCount;
